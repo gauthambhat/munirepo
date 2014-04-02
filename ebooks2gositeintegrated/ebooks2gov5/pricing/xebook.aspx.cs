f3dce071a16acaa9cdf6e977af69d6689a4b6015
gauthambhat <gauthambhat.dv@gmail.com>
2014-04-02 17:28:13 +0530

    by gautham changes are donme

--- ebooks2gositeintegrated/ebooks2gov5/pricing/xebook.aspx.cs
+++ ebooks2gositeintegrated/ebooks2gov5/pricing/xebook.aspx.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Web.UI.WebControls;// to get webcontrols
 
 using System.Data;  //To get the datatable object need to add this namespace.
 
